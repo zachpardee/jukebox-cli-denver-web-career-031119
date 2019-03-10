771addbb3b6ef514d39eb20a164538661731987e
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-10 20:49:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,11 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def say_hello(name)
+  "Hi #{name}!"
+end
+ 
+puts "Enter your name:"
+users_name = gets.chomp
+ 
+puts say_hello(users_name)
